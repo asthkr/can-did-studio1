09ac00cf6bb916c8d258abe0152bffce553e1245
asthkr <127844584+asthkr@users.noreply.github.com>
2023-12-01 14:13:14 +0530

    final

--- main.js
+++ main.js
@@ -1,95 +1,110 @@
+$(window).scroll(function () {
+  var header = $(".header"),
+    scroll = $(window).scrollTop();
 
+  if (scroll >= 50) header.addClass("fixed");
+  else header.removeClass("fixed");
+});
+$(".nav-icon").click(function () {
+  $(".nav-bar").toggleClass("d-none");
+});
 const imgzoom2 = document.querySelectorAll("#gallery-anime .gallery-item");
 
 imgzoom2.forEach((element) => {
-	const contents = element.querySelectorAll(".contentgallery") 
+  const contents = element.querySelectorAll(".contentgallery");
 
-    gsap.set(contents, { scale:.5,
-    yPercent:50, });
+  gsap.set(contents, { scale: 0.5, yPercent: 50 });
 
-    gsap.to(contents, { 
-        yPercent:0, 
-        duration: 1.2,
-        autoAlpha:1,
-        scale: 1,
-        ease: "power2.out",
-        stagger:2.5,
-        scrollTrigger: {   
-            trigger: element,
-            start: "top bottom-=30%",
-            end: "bottom top+=100",
-            toggleActions: "play reverse play reverse"
-        }
-    });
-})
-
-
-const tl = gsap.timeline({
+  gsap.to(contents, {
+    yPercent: 0,
+    duration: 1.2,
+    autoAlpha: 1,
+    scale: 1,
+    ease: "power2.out",
+    stagger: 2.5,
     scrollTrigger: {
-      scrub: 1,
-      trigger: "#blogbox1",
-      start: "top bottom+=100",
-      end: "bottom bottom",
-      markers:false,
+      trigger: element,
+      start: "top bottom-=30%",
+      end: "bottom top+=100",
+      toggleActions: "play reverse play reverse",
     },
   });
-  
-  tl.fromTo("#blogbox1", {
-    y:100,
+});
 
-  opacity
-  :.7,
-  scale:.6,
+const tl = gsap.timeline({
+  scrollTrigger: {
+    scrub: 1,
+    trigger: "#blogbox1",
+    start: "top bottom",
+    end: "bottom bottom",
+    markers:false,
   },
-  {y:0,
-   
-      opacity:1,
-      scale:1,
-    });
+});
 
-    const tl2 = gsap.timeline({
-        scrollTrigger: {
-          scrub: 1,
-          trigger: "#blogbox2",
-          start: "top bottom",
-          end: "bottom bottom",
-          markers:false,
-        },
-      });
-  
-      tl2.fromTo("#blogbox2", {
-        y:100,
-    
-      opacity
-      :.7,
-      scale:.6,
-      },
-      {y:0,
-       
-          opacity:1,
-          scale:1,
-        });
-   
+tl.fromTo(
+  ".img-box1",
+  {
+    y: 100,
+x:-200,
+    opacity: 0.7,
+    scale: 0.6,
+  },
+  {x:0,
+    y: 0,
+    opacity: 1,
+    scale: 1,
+  }
+);
 
-        const tl3 = gsap.timeline({
-            scrollTrigger: {
-              scrub: 1,
-              trigger: "#blogbox3",
-              start: "top bottom",
-              end: "bottom bottom",
-              markers:false,
-            },
-          });
-          
-          tl.fromTo("#blogbox3", {
-            y:100,
-        
-          opacity
-          :.7,
-          scale:.6,
-          },
-          {y:0,
-           
-              opacity:1,
-              scale:1,
-            });
+const tl2 = gsap.timeline({
+  scrollTrigger: {
+    scrub: 1,
+    trigger: "#blogbox2",
+    start: "top bottom",
+    end: "bottom bottom",
+    markers:false,
+  },
+});
+
+tl2.fromTo(
+  ".img-box2",
+  {
+    y: 200,
+
+    opacity: 0.7,
+    scale: 0.6,
+  },
+  {
+    y: 0,
+
+    opacity: 1,
+    scale: 1,
+  }
+);
+
+const tl3 = gsap.timeline({
+  scrollTrigger: {
+    scrub: 1,
+    trigger: "#blogbox3",
+    start: "top bottom",
+    end: "bottom bottom",
+    markers:false,
+
+  },
+});
+
+tl3.fromTo(
+  ".img-box3",
+  {
+    y: 100,
+x:200,
+    opacity: 0.7,
+    scale: 0.6,
+  },
+  {
+    y: 0,
+x:0,
+    opacity: 1,
+    scale: 1,
+  }
+);
